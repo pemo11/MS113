75e5d8bef88cfc491066dffe71b4ade590bd5c67
Peter Monadjemi <pm@eureka-fach.de>
2024-10-28 09:17:07 +0100

    Übungen, Pdf und Pptx

--- Beispiele/Uebungen/PCAlter_Uebung/PCAlter_Step02.ps1
+++ Beispiele/Uebungen/PCAlter_Uebung/PCAlter_Step02.ps1
@@ -3,8 +3,7 @@
  Projekt PC-Alter-Abfrage
  .Notes
  Schritt 2: Die Abfrage wird in eine Function eingebaut
- 
- Da die Function  noch ohne Parameter verwendet wird, bleibt alles ganz einfach
+ Da die Function noch ohne Parameter verwendet wird, bleibt alles ganz einfach
 #>
 
 function Get-CPUTyp
